c7fd14cbd7d03cabdb7a9d06c9014819d7a1217e
Nakiyah Fears <fkiyah@yahoo.com>
2021-04-13 22:47:41 -0400

    Initial commit

---
+++ src/serviceWorker.js
@@ -0,0 +1,17 @@
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      window.location.hostname === '[::1]' ||
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+  
+export function register(config) {
+    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+      const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+      if (publicUrl.origin !== window.location.origin) {
+        return;
+      }
+    }
+}
+  
